0180a2c5473578be22cf251c4be255411e690ce9
Lydia Lima <lydialima51@gmail.com>
2020-04-06 21:15:59 -0300

    Criação do arquivo index.html

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,11 @@
-alert('Olá mundo!')
+function botao1() {
+    alert('Mensagem do dia do mestre Picão:\n "É fazendo merda que se aduba a vida". \n Até a próxima, pessoal!');
+}
+
+function botao2() {
+    alert('Mensagem do dia do mestre Picão:\n "Se você cair, eu estarei aqui. - Disse o chão". \n Até a próxima, pessoal!');
+}
+
+function botao3() {
+    alert('Mensagem do dia do mestre Picão:\n "Eu até ligaria para a sua opinião mas estou sem créditos". \n Até a próxima, pessoal!')
+}
